fc6af3dd964091d4795a30b8f7f05c1a3dcfbfb9
Татьяна Кызтек <k.davud@yandex.ru>
2021-07-24 10:07:13 +0600

    StudentClass

---
+++ src/Student.java
@@ -0,0 +1,79 @@
+public class Student {
+        private String studentName;
+        private String studentLastname;
+        private String course;
+        private Long studentid;
+        private Integer age;
+        private Integer studentyear;
+
+        public Student(String studentName, String studentLastname, String course, Long studentid, Integer age, Integer studentyear) {
+            this.studentName = studentName;
+            this.studentLastname = studentLastname;
+            this.course = course;
+            this.studentid = studentid;
+            this.age = age;
+            this.studentyear = studentyear;
+        }
+
+
+        public String getStudentName() {
+            return studentName;
+        }
+
+        public void setStudentName(String studentName) {
+            this.studentName = studentName;
+        }
+
+        public String getStudentLastname() {
+            return studentLastname;
+        }
+
+        public void setStudentLastname(String studentLastname) {
+            this.studentLastname = studentLastname;
+        }
+
+        public String getCourse() {
+            return course;
+        }
+
+        public void setCourse(String course) {
+            this.course = course;
+        }
+
+        public Long getStudentid() {
+            return studentid;
+        }
+
+        public void setStudentid(Long studentid) {
+            this.studentid = studentid;
+        }
+
+        public Integer getAge() {
+            return age;
+        }
+
+        public void setAge(Integer age) {
+            this.age = age;
+        }
+
+        public Integer getStudentyear() {
+            return studentyear;
+        }
+
+        public void setStudentyear(Integer studentyear) {
+            this.studentyear = studentyear;
+        }
+
+        @Override
+        public String toString() {
+            return "Student{" +
+                    "studentName='" + studentName + '\'' +
+                    ", studentLastname='" + studentLastname + '\'' +
+                    ", course='" + course + '\'' +
+                    ", studentid=" + studentid +
+                    ", age=" + age +
+                    ", studentyear=" + studentyear +
+                    '}';
+        }
+    }
+
