fa5d76a5cbcdece29b39efffb8bdd58ce89b21fd
NodirUmarov <mr.umarov.1995@gmail.com>
2021-07-24 10:28:49 +0600

    Controler class added but not finished

---
+++ src/Controller.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Controller {
+
+    private List<User> users = new ArrayList<>();
+
+    public void run() {
+        while(true) {
+            register();
+        }
+    }
+
+    private void register() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Email: ");
+        String email = scan.nextLine();
+
+        System.out.print("Password");
+        String password = scan.nextLine();
+
+        User user = new User(email, password);
+        users.add(user);
+    }
+
+    
+
+}
